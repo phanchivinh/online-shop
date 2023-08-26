9416c62b8c04bd0341c0f54c0dc415bfd5517724
Vinh Phan <phanchivinh.7275@gmail.com>
2023-08-26 09:57:40 +0700

    update

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { LiaCertificateSolid } from 'react-icons/lia'
+// import { LiaCertificateSolid } from 'react-icons/lia'
 import { GiCheckMark } from 'react-icons/gi'
 import { TbTruckDelivery } from 'react-icons/tb'
 
 const Announcement = () => {
   return (
     <div className='h-7 bg-blue-400 text-white font-extrabold flex items-center justify-center text-sm'>
-      <LiaCertificateSolid className='mr-2 ml-3 text-2xl' /> Đảm bảo chất lượng
+      Đảm bảo chất lượng
       <GiCheckMark className='mr-2 ml-3 text-2xl' /> Mở kiểm tra nhận hàng
       <TbTruckDelivery className='mr-2 ml-3 text-2xl' /> Vận chuyển siêu tốc
     </div>
